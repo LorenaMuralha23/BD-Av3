071e65b36aab9994dd773eeed848c44b7ca4b890
luisvicarri <vicarrif.luis@gmail.com>
2024-08-13 19:33:05 -0300

    database completed

--- BD/views.sql
+++ BD/views.sql
@@ -1,4 +1,4 @@
 /*Mesas disponíveis no dia de hoje*/
 CREATE OR REPLACE VIEW Free_Tables_Today AS
 SELECT tb_table.number AS table_number, tb_table.capacity AS table_capacity, tb_table.location AS table_location 
-FROM tb_table LEFT JOIN tb_reservation ON tb_table.id = tb_reservation.id_table AND tb_reservation.reservation_date = CURDATE() WHERE tb_reservation.id_table IS NULL;  
+FROM tb_table LEFT JOIN tb_reservation ON tb_table.id = tb_reservation.id_table AND tb_reservation.reservation_date = CURDATE() WHERE tb_reservation.id_table IS NULL;
